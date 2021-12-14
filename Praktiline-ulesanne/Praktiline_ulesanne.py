17ea52bdd703eb3cd5dde866b7cdcb81b2ffeef8
Эдгар Неверовский <edgarneverovskij@gmail.com>
2021-12-15 00:00:10 +0200

    Valmis töö

--- Praktiline-ulesanne/Praktiline_ulesanne.py
+++ Praktiline-ulesanne/Praktiline_ulesanne.py
@@ -11,16 +11,16 @@ while True:
     except ValueError:
         print("Vale andmetüüp!")
 if vastus==1:
-    for i in range(10):
-        spisok1.append(input("Palun sisetage väärtust - "))
+    for i in range(3):
+        spisok1.append(input("Palun sisetage väärtus - "))
     while vastus2!=0:
         try:
             print("1 - Näidata loendi")
-            print("2 - Kontroll, kas loend sisaldab nubri")
-            print("3 - Kontroll, kas loend sisaldab tähti")
+            print("2 - Näidata loendi sortimist")
+            print("3 - Näidata loendi suurim arv")
             print("4 - Näidata loendi ümber pööramist")
-            print("5 - Näidata loendi, mis koosneb suurte algustähtedest")
-            print("6 - Näidata loendi, mis koosneb väikste algustähtedest")
+            print("5 - Loengute ühendamine")
+            print("6 - Loengu kopeerimine")
             print("7 - Lisada element loendi lõppu")
             print("8 - Kustutada loendi esimest väärtust")
             print("9 - Näidata loendi pikkust")
@@ -33,19 +33,18 @@ if vastus==1:
             if tehe==1:
                 print(spisok1)
             elif tehe==2:
-                spisok1.isdigit()
+                spisok1.sort()
                 print(spisok1)
             elif tehe==3:
-                spisok1.isalpha()
-                print(spisok1)
+                print(max(spisok1))
             elif tehe==4:
                 spisok1.reverse()
                 print(spisok1)
             elif tehe==5:
-                spisok1.isupper()
+                spisok1.extend(spisok0)
                 print(spisok1)
             elif tehe==6:
-                spisok1.islower()
+                spisok1.copy()
                 print(spisok1)
             elif tehe==7:
                 spisok1.append(input("Sisetage element, mis tahate lisada - "))
@@ -62,6 +61,61 @@ if vastus==1:
             while True:
                 try:
                     vastus2=int(input("Sisetage vastus - "))
+                    if vastus2==1 or vastus2==0: break
+                except ValueError:
+                    print("Vale andmetüüp!")
+        except ValueError:
+            print("Aitäh töötamise eest!")
+else:
+    while vastus2!=0:
+        try:
+            print("1 - Näidata loendi")
+            print("2 - Näidata loendi sortimist")
+            print("3 - Näidata loendi suurim arv")
+            print("4 - Näidata loendi ümber pööramist")
+            print("5 - Loengute ühendamine")
+            print("6 - Loengu kopeerimine")
+            print("7 - Lisada element loendi lõppu")
+            print("8 - Kustutada loendi esimest väärtust")
+            print("9 - Näidata loendi pikkust")
+            print("10 - Puhastada loendit")
+            while tehe!=int and tehe not in [1,2,3,4,5,6,7,8,9,10]:
+                try:
+                    tehe=int(input("Palun kirjutage mida tahate teha - "))
+                except ValueError:
+                    print("Vale andmetüüp!")
+            if tehe==1:
+                print(spisok0)
+            elif tehe==2:
+                spisok0.sort()
+                print(spisok0)
+            elif tehe==3:
+                print(max(spisok0))
+            elif tehe==4:
+                spisok0.reverse()
+                print(spisok0)
+            elif tehe==5:
+                spisok0.extend(spisok0)
+                print(spisok0)
+            elif tehe==6:
+                spisok0.copy()
+                print(spisok0)
+            elif tehe==7:
+                spisok0.append(input("Sisetage element, mis tahate lisada - "))
+                print(spisok0)
+            elif tehe==8:
+                spisok0.pop(0)
+                print(spisok0)            
+            elif tehe==9:
+                print(len(spisok0))
+            elif tehe==10:
+                spisok0.clear()
+                print(spisok0)
+            print("Kas tahate jätkata? (0 - Ei/1 - Jah)")
+            while True:
+                try:
+                    vastus2=int(input("Sisetage vastus - "))
+                    if vastus2==1 or vastus2==0: break
                 except ValueError:
                     print("Vale andmetüüp!")
         except ValueError:
