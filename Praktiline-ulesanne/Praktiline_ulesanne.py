3a5b836bad45ced2f926e0402bad5cb857fcbf32
Эдгар Неверовский <edgarneverovskij@gmail.com>
2021-12-15 22:25:44 +0200

    Uued funktsioonid

--- Praktiline-ulesanne/Praktiline_ulesanne.py
+++ Praktiline-ulesanne/Praktiline_ulesanne.py
@@ -2,7 +2,10 @@
 spisok1=[]
 tehe=""
 vastus2=""
+slovo0="Portugaalia"
+slovo1=""
 print("Praktiline ülesanne loenditega")
+print("Mina mõtlesin välja sõna Portugaalia")
 print("Kas te tahate luua oma loendi, mis koosneb 10 elementist või kasutada minu loendi? (0 - minu /1 - uus)")
 while True:
     try:
@@ -13,6 +16,7 @@ while True:
 if vastus==1:
     for i in range(3):
         spisok1.append(input("Palun sisetage väärtus - "))
+    slovo1=input("Palun mõtle välja sõna - ")
     while vastus2!=0:
         try:
             print("1 - Näidata loendi")
@@ -25,38 +29,55 @@ if vastus==1:
             print("8 - Kustutada loendi esimest väärtust")
             print("9 - Näidata loendi pikkust")
             print("10 - Puhastada loendit")
-            while tehe!=int and tehe not in [1,2,3,4,5,6,7,8,9,10]:
+            print(f"11 - Kontroll, kas sõnas {slovo1} on numbrid")
+            print(f"12 - Kontroll, kas sõnas {slovo1} on tähed")
+            print(f"13 - Kontroll, kas sõna {slovo1} algab suure algustähega")
+            print("14 - Teisendada esimest tähte suurtäheks ja kõik teised väiketähtedeks")
+
+            while tehe!=int and tehe not in [1,2,3,4,5,6,7,8,9,10,11,12,13,14]:
                 try:
                     tehe=int(input("Palun kirjutage mida tahate teha - "))
                 except ValueError:
                     print("Vale andmetüüp!")
             if tehe==1:
                 print(spisok1)
-            elif tehe==2:
-                spisok1.sort()
-                print(spisok1)
+            elif tehe==2:                
+                print(spisok1.sort())
             elif tehe==3:
                 print(max(spisok1))
-            elif tehe==4:
-                spisok1.reverse()
-                print(spisok1)
-            elif tehe==5:
-                spisok1.extend(spisok0)
-                print(spisok1)
-            elif tehe==6:
-                spisok1.copy()
-                print(spisok1)
+            elif tehe==4:                
+                print(spisok1.reverse())
+            elif tehe==5:                
+                print(spisok1.extend(spisok0))
+            elif tehe==6:               
+                print(spisok1.copy())
             elif tehe==7:
                 spisok1.append(input("Sisetage element, mis tahate lisada - "))
                 print(spisok1)
-            elif tehe==8:
-                spisok1.pop(0)
-                print(spisok1)            
+            elif tehe==8:               
+                print(spisok1.pop(0))            
             elif tehe==9:
                 print(len(spisok1))
             elif tehe==10:
                 spisok1.clear()
                 print(spisok1)
+            elif tehe==11:
+                if slovo1.isdigit()==True:
+                    print(f"Sõna {slovo1} sisaldab numbri")
+                else:
+                    print(f"Sõna {slovo1} ei sisalda numbri")
+            elif tehe==12:
+                if slovo1.isalpha()==True:
+                    print(f"Sõna {slovo1} sisaldab tähti")
+                else:
+                    print(f"Sõna {slovo1} ei sisalda tähti")
+            elif tehe==13:
+                if slovo1.istitle()==True:
+                    print(f"Sõna {slovo1} algab suure algustähega")
+                else:
+                    print(f"Sõna {slovo1} algab väikse algustähega")
+            elif tehe==14:
+                print(slovo1.capitalize())
             print("Kas tahate jätkata? (0 - Ei/1 - Jah)")
             while True:
                 try:
@@ -79,38 +100,54 @@ else:
             print("8 - Kustutada loendi esimest väärtust")
             print("9 - Näidata loendi pikkust")
             print("10 - Puhastada loendit")
-            while tehe!=int and tehe not in [1,2,3,4,5,6,7,8,9,10]:
+            print(f"11 - Kontroll, kas sõnas {slovo0} on numbrid")
+            print(f"12 - Kontroll, kas sõnas {slovo0} on tähed")
+            print(f"13 - Kontroll, kas sõna {slovo0} algab suure algustähega")
+            print("14 - Teisendada esimest tähte suurtäheks ja kõik teised väiketähtedeks")
+            while tehe!=int and tehe not in [1,2,3,4,5,6,7,8,9,10,11,12,13,14]:
                 try:
-                    tehe=int(input("Palun kirjutage mida tahate teha - "))
+                   tehe=int(input("Palun kirjutage mida tahate teha - "))
                 except ValueError:
-                    print("Vale andmetüüp!")
+                   print("Vale andmetüüp!")
             if tehe==1:
                 print(spisok0)
-            elif tehe==2:
-                spisok0.sort()
-                print(spisok0)
+            elif tehe==2:                
+                print(spisok0.sort())
             elif tehe==3:
                 print(max(spisok0))
-            elif tehe==4:
-                spisok0.reverse()
-                print(spisok0)
-            elif tehe==5:
-                spisok0.extend(spisok0)
-                print(spisok0)
-            elif tehe==6:
-                spisok0.copy()
-                print(spisok0)
+            elif tehe==4:                
+                print(spisok0.reverse())
+            elif tehe==5:                
+                print(spisok0.extend(spisok0))
+            elif tehe==6:               
+                print(spisok0.copy())
             elif tehe==7:
                 spisok0.append(input("Sisetage element, mis tahate lisada - "))
                 print(spisok0)
-            elif tehe==8:
-                spisok0.pop(0)
-                print(spisok0)            
+            elif tehe==8:               
+                print(spisok0.pop(0))            
             elif tehe==9:
                 print(len(spisok0))
             elif tehe==10:
                 spisok0.clear()
                 print(spisok0)
+            elif tehe==11:
+                if slovo0.isdigit()==True:
+                    print(f"Sõna {slovo0} sisaldab numbri")
+                else:
+                    print(f"Sõna {slovo0} ei sisalda numbri")
+            elif tehe==12:
+                if slovo0.isalpha()==True:
+                    print(f"Sõna {slovo0} sisaldab tähti")
+                else:
+                    print(f"Sõna {slovo0} ei sisalda tähti")
+            elif tehe==13:
+                if slovo0.istitle()==True:
+                    print(f"Sõna {slovo0} algab suure algustähega")
+                else:
+                    print(f"Sõna {slovo0} algab väikse algustähega")
+            elif tehe==14:
+                    print(slovo0.capitalize())
             print("Kas tahate jätkata? (0 - Ei/1 - Jah)")
             while True:
                 try:
